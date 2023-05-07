54b48b712c00e29b4c02d4f1cfbc649f500f0c5b
augustin j <86960242+augustin7698@users.noreply.github.com>
2023-05-07 21:13:34 +0200

    Update table.js

--- table.js
+++ table.js
@@ -1,23 +1,18 @@
-tableLink = [
-	"https://embed-of-my-video.mp4",
-	"https://embed-of-my-video.fr",
-	"https://embed-of-my-video2.com"
-];
-
-tablePicture = [
-	"https://img.com/src/film.jpg",
-	"myimg.png",
-	"gif.gif"
-];
-
-tableName = [
-	"My Animation",
-	"Film name",
-	"Documentaire"
-];
-
-tableLabel = [
-	"animation",
-	"film",
-	"documentaire"
-];
+movies = [
+	{
+	    "name": "My Animation",
+	    "label": "animation",
+	    "link": "https://embed-of-my-video.mp4",
+	    "picture": "https://img.com/src/film.jpg"
+	},{
+	    "name": "Film name",
+	    "label": "film",
+	    "link": "https://embed-of-my-video.fr",
+	    "picture": "myimg.png"
+	},{
+	    "name": "Documentaire",
+	    "label": "documentaire",
+	    "link": "https://embed-of-my-video2.com",
+	    "picture": "gif.gif"
+	}
+]
